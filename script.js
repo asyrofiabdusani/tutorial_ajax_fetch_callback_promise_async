d962a11ed09b2271ae83c2f86ad007a6e213f8d2
Asyrofi Abdusani <asyrofiabdusani@pop-os.localdomain>
2022-09-28 00:27:21 +0700

    using promise

--- script.js
+++ script.js
@@ -1,48 +1,59 @@
-const btSrc = document.querySelector('.bt-search');
-const keyword = document.querySelector('.in-key');
-const tblBd = document.querySelector('.tbl-body');
+const btSrc = document.querySelector(".bt-search");
+const keyword = document.querySelector(".in-key");
+const tblBd = document.querySelector(".tbl-body");
 
-btSrc.addEventListener('click', ()=>getRslt());
+btSrc.addEventListener("click", () => {
+    clPage();
+    const prom = getRslt(getKey());
+    prom.then((data) => {
+        if (data.rowCount > 0) {
+            disRsl(data.hero);
+        } else {
+            keyWrong();
+        }
+    });
+});
+
+function getRslt(url) {
+    let prom = new Promise(function (resolve, reject) {
+        const ajax = new XMLHttpRequest();
+        ajax.onload = function () {
+            if (ajax.status === 200) {
+                let data = JSON.parse(ajax.responseText);
+                resolve(data);
+            } else {
+                reject(shErr());
+            }
+        };
+        ajax.open("GET", url);
+        ajax.send();
+    });
+    return prom;
+}
 
 function getKey() {
-    const url = 'https://api.dazelpro.com/mobile-legends/role?roleName='+keyword.value;
+    const url =
+        "https://api.dazelpro.com/mobile-legends/role?roleName=" +
+        keyword.value;
     return url;
 }
 
-function getRslt() {
-    clPg();
-    const url = getKey();
-    const ajax = new XMLHttpRequest();
-    ajax.onload = function () {
-        const data = JSON.parse(ajax.responseText);
-        if (data.status ===200 && data.rowCount > 0) {
-            disRsl(data.hero);
-        } else if (data.status ===200 && data.rowCount === 0) {
-            keyWrong();
-        } else {
-            shErr();
-        }
-    }
-    ajax.open('GET', url);
-    ajax.send();
-}
-
 function disRsl(data) {
-    let hero = '';
-    data.map(e => {
+    let hero = "";
+    data.map((e) => {
         hero += `<tr>
-                    <td>${e['hero_name']}</td>
-                    <td>${e['hero_role']}</td>
-                    <td>${e['hero_specially']}</td>
-                    <td><img src="${e['hero_avatar']}"></td>
-                    <td><button class="bt-detail" value = ${e['hero_id']}>Detail</button></td>
+                    <td>${e["hero_name"]}</td>
+                    <td>${e["hero_role"]}</td>
+                    <td>${e["hero_specially"]}</td>
+                    <td><img src="${e["hero_avatar"]}"></td>
+                    <td><button class="bt-detail" value = ${e["hero_id"]}>Detail</button></td>
                 </tr>`;
     });
     tblBd.innerHTML = hero;
 }
 
-function clPg() {
-    tblBd.innerHTML='';
+function clPage() {
+    tblBd.innerHTML = "";
 }
 
 function keyWrong() {
@@ -50,5 +61,5 @@ function keyWrong() {
 }
 
 function shErr() {
-    alert('There is something wrong, please retry!');
+    alert("There is something wrong, please retry!");
 }
