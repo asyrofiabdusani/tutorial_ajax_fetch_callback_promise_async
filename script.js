ae0ee4225661dcd7770f851c1948056d76c7526e
Asyrofi Abdusani <asyrofiabdusani@pop-os.localdomain>
2022-09-28 21:11:11 +0700

    adding detail feature

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const btSrc = document.querySelector(".bt-search");
 const keyword = document.querySelector(".in-key");
 const tblBd = document.querySelector(".tbl-body");
+const dtlCont = document.querySelector(".detail-cont");
 
 btSrc.addEventListener("click", async () => {
     clPage();
@@ -12,7 +13,8 @@ btSrc.addEventListener("click", async () => {
         } else {
             shErr("You input wrong keyword");
         }
-    } catch {
+    } catch (err) {
+        console.log(err);
         shErr("There is something wrong, please retry!");
     } finally {
         console.log("Done");
@@ -43,6 +45,8 @@ function disRsl(data) {
                 </tr>`;
     });
     tblBd.innerHTML = hero;
+    const btDtl = document.querySelectorAll(".bt-detail");
+    disDtl(btDtl);
 }
 
 function clPage() {
@@ -51,4 +55,64 @@ function clPage() {
 
 function shErr(msg) {
     alert(msg);
+}
+
+function disDtl(bt) {
+    bt.forEach((e) => {
+        e.addEventListener("click", async () => {
+            const response = await getDtl(getDtlUrl(e.value));
+            const dtl = await response.json();
+            shDtl(dtl.hero[0]);
+        });
+    });
+}
+
+function shDtl(data) {
+    let dtlCtn = `<table class="tb-dtl">
+                    <thead>
+                        <tr>
+                            <th colspan="2">Summary</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>Name</th>
+                            <td>${data["hero_name"]}</td>
+                        </tr>
+                        <tr>
+                            <th>Role</th>
+                            <td>${data["hero_role"]}</td>
+                        </tr>
+                        <tr>
+                            <th>Specially</th>
+                            <td>${data["hero_specially"]}</td>
+                        </tr>
+                        <tr>
+                            <th>Image</th>
+                            <td><img src="${data["hero_avatar"]}" alt="" /></td>
+                        </tr>
+                        <tr>
+                            <th colspan="2"><button  class="bt-close">close</button></th>
+                        </tr>
+                    </tbody>
+                </table>`;
+
+    dtlCont.setAttribute(
+        "style",
+        "width: 100%; height: 100vh; background-color: rgba(255, 255, 255, 0.8); position: fixed;display: flex; align-items: center;padding-left: 30%;"
+    );
+    dtlCont.innerHTML = dtlCtn;
+    const btCl = document.querySelector(".bt-close");
+    btCl.addEventListener("click", () =>
+        dtlCont.setAttribute("style", "display:none")
+    );
+}
+
+function getDtl(url) {
+    return fetch(url);
+}
+
+function getDtlUrl(id) {
+    const url = "https://api.dazelpro.com/mobile-legends/hero/" + id;
+    return url;
 }
